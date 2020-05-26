209a467dca1f7d33b501c0ab99cb77848169bded
aysa <j.aysa@yahoo.com>
2020-05-26 13:09:24 +0000

    database with insert sample for testing

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
-
 CREATE TABLE services(
   service_id serial PRIMARY KEY,
   service_name VARCHAR (150) UNIQUE NOT NULL,
   description VARCHAR (15000) NOT NULL,
+  short_description VARCHAR (500) Not NULL,
   photo_address VARCHAR (50) not null
 );
 
@@ -18,10 +18,11 @@ CREATE TABLE events(
   staff_id serial REFERENCES staff(staff_id) ,
   event_name VARCHAR (50) UNIQUE NOT NULL,
   description VARCHAR (15000) NOT NULL,
+  short_description VARCHAR (500) Not NULL,
   location VARCHAR (150) NOT NULL,
   time_event TIMESTAMP NOT NULL,
   date_event date not null,
-    photo_address VARCHAR (50) not null
+  photo_address VARCHAR (50) not null
 );
 
 CREATE TABLE Involve(
@@ -47,3 +48,33 @@ CREATE TABLE present(
       REFERENCES events (event_id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+INSERT INTO services (service_id, service_name, description, short_description, photo_address) 
+VALUES 
+                      (1, 'Hospice', 'XX','XXX', 'X.jpg');
+                      
+select * from services;  
+
+INSERT INTO staff (staff_id, staff_name, staff_role, photo_address) 
+VALUES 
+                      (1, 'Andrea Andreoni', 'XX', 'XX.jpg');
+                      
+select * from staff;
+
+INSERT INTO events (event_id, staff_id, event_name, description,short_description, location, time_event,date_event, photo_address) 
+VALUES 
+                      (1, 'Andrea Andreoni', 'XX','XXX', 'XX.jpg','XX','XX','XX','XX');
+                      
+select * from events;
+
+INSERT INTO Involve (staff_id, service_id) 
+VALUES 
+                      (1, 1);
+                      
+select * from Involve;
+
+INSERT INTO present (event_id, service_id) 
+VALUES 
+                      (1, 1);
+                      
+select * from present;
