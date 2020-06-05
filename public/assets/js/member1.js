c2302d4a130b3b9269f353d1d060cb04e651c1d7
MOHAMMAD ALA AMJADI <maa4517@gmail.com>
2020-06-05 23:34:12 +0200

    member.js finished

--- public/assets/js/member1.js
+++ public/assets/js/member1.js
@@ -1,31 +1,61 @@
-const urlParams =window.location.search.split('=');
+const URL = {
+  PERSON: {
+    DEFAULT: "https://aysa-ojoloo.herokuapp.com/api/person/",
+    EVENTS: "/events",
+    SERVICES: "/services",
+  },
+};
 
-loadDoc("https://aysa-ojoloo.herokuapp.com/api/person/"+urlParams[1], function_person_member);
-var i
+const urlParams = window.location.search.split("=")
+loadDoc(URL.PERSON.DEFAULT + urlParams[1], function_person_member)
+
+loadDoc(URL.PERSON.DEFAULT + urlParams[1] + URL.PERSON.EVENTS, function_member_events);
+
+loadDoc(URL.PERSON.DEFAULT + urlParams[1] + URL.PERSON.SERVICES, function_member_services);
+
+function function_member_events(result) {
+  if (result.length!=0) {
+    result.forEach(element => {
+      document.getElementById("getEventsFromAPI").innerHTML += `<a class="cus_link" href="event.html?event_id=${element.event_id}"><span>${element.event_name}</span></a>`;
+    });
+  } else {
+    document.getElementById("getEventsFromAPI").innerHTML += `<p><span>Not involved in any events</span></p>`;
+  }
+}
+
+function function_member_services(result) {
+  if (result.length!=0) {
+    result.forEach(element => {
+      document.getElementById("getServicesFromAPI").innerHTML += `<a class="cus_link" href="service.html?service_id=${element.service_id}"><span>${element.service_name}</span></a>`;
+    });
+  } else {
+    document.getElementById("getServicesFromAPI").innerHTML += `<p><span>Not involved in any services</span></p>`;
+  }
+}
+
+var i;
 function loadDoc(url, cFunction) {
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       const result = JSON.parse(this.responseText);
-      document.title = ` ${result.person_name}`;
-      console.log(urlParams[1])
+      /* console.log(urlParams[1]) */
       cFunction(result);
     }
   };
-  xhttp.open("GET", url, true)
+  xhttp.open("GET", url, true);
   xhttp.send();
-
 }
 
 function function_person_member(result) {
   // action goes here
-  console.log(result)
+  /* console.log(result) */
+  document.title = ` ${result.person_name}`;
   var desc = JSON.parse(result.long_description);
-  console.log(desc)
-  for(i=0;i<6;i++){
-      if(urlParams[1] == i){
-        
-      document.getElementById("person_name1").innerHTML =  `
+  /* console.log(desc) */
+  for (i = 0; i < 6; i++) {
+    if (urlParams[1] == i) {
+      document.getElementById("person_name1").innerHTML = `
             <div
               class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 align-items-start"
             >
@@ -43,26 +73,18 @@ function function_person_member(result) {
               />
               <p class="cus_present mt-3">
                 <strong>${desc[0]} </strong>
-                <a class="cus_link" href="#">
-                  <span>
-                    Spring event
-                  </span>
-                </a>
+                <div id="getEventsFromAPI"></div>
               </p>
               <p>
                 <strong>${desc[1]}</strong>
-                <a href="#">
-                  <span>
-                    Hospice service
-                  </span>
-                </a>
+                <div id="getServicesFromAPI"></div>
               </p>
               
             </div>
             <div
               class="col-12 col-sm-6 col-md-6 col-lg-8 col-xl-8 align-items-start"
             >
-              <p class="cus_title_justified">${result.person_role}</p>
+              <p class="cus_title_justified">Role: ${result.person_role}</p>
               <p class="cus_content">
                 ${desc[2]}
               </p>
@@ -85,8 +107,7 @@ function function_person_member(result) {
               </p>
             </div>
  
-          `
-      }
+          `;
     }
-
-};
+  }
+}
