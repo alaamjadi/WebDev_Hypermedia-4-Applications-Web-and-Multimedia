d5691708778ef7d2c38ce963d2a2f6fc5fb9d7f2
MOHAMMAD ALA AMJADI <maa4517@gmail.com>
2020-06-06 04:49:21 +0200

    variable changes for member.js, page connections for service.js

--- public/assets/js/member.js
+++ public/assets/js/member.js
@@ -1,13 +1,12 @@
 const URL = {
-  PERSON: {
-    ID: window.location.search.split("=")[1],
-    DEFAULT: "https://aysa-ojoloo.herokuapp.com/api/person/",
-    EVENTS: "/events",
-    SERVICES: "/services",
-  },
+  ID:       '/' + window.location.search.split("=")[1],
+  API:      "https://aysa-ojoloo.herokuapp.com/api",
+  EVENTS:   "/events",
+  PERSON:   "/person",
+  SERVICES: "/services"
 };
 
-fetch(URL.PERSON.DEFAULT + URL.PERSON.ID)
+fetch(URL.API + URL.PERSON + URL.ID)
   .then(function (response) {
     if (response.status !== 200) {
       console.log("Fetch response failed. Status Code: " + response.status);
@@ -22,7 +21,7 @@ fetch(URL.PERSON.DEFAULT + URL.PERSON.ID)
     document.getElementById(
       "person_name1"
     ).innerHTML = `<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 align-items-start"><h1>${data.person_name}</h1></div><div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 align-items-start" id = "person_pic1"> <img src="../assets/img/${data.photo_address}" class="img-fluid" alt="staffpics"/><p class="cus_present mt-3"><strong>${L_description[0]} </strong><div id="getEventsFromAPI"></div></p><p><strong>${L_description[1]}</strong><div id="getServicesFromAPI"></div></p></div><div class="col-12 col-sm-6 col-md-6 col-lg-8 col-xl-8 align-items-start"><p class="cus_title_justified">Role: ${data.person_role}</p><p class="cus_content">${L_description[2]}</p><p class="mb-1 mt-4 cus_title_justified">${L_description[3]}</p><p class="cus_content">${L_description[4]}</p><p class="mb-0 mt-4 cus_title_justified">${L_description[5]}</p><p class="cus_content">${L_description[6]}</p><p class="mb-1 mt-4 cus_title_justified">${L_description[7]}</p><p class="cus_content">${L_description[8]}</p></div>`;
-    return fetch(URL.PERSON.DEFAULT + URL.PERSON.ID + URL.PERSON.EVENTS);
+    return fetch(URL.API + URL.PERSON + URL.ID + URL.EVENTS);
   })
   .then(function (response) {
     if (response.status !== 200) {
@@ -44,7 +43,7 @@ fetch(URL.PERSON.DEFAULT + URL.PERSON.ID)
         "getEventsFromAPI"
       ).innerHTML += `<p><span>Not involved in any events</span></p>`;
     }
-    return fetch(URL.PERSON.DEFAULT + URL.PERSON.ID + URL.PERSON.SERVICES);
+    return fetch(URL.API + URL.PERSON + URL.ID + URL.SERVICES);
   })
   .then(function (response) {
     if (response.status !== 200) {
--- public/assets/js/service.js
+++ public/assets/js/service.js
@@ -1,128 +1,82 @@
-
-const urlParams =window.location.search.split('=');
-console.log(window.location.search.split('='))
-loadDoc("https://aysa-ojoloo.herokuapp.com/api/Services/"+urlParams[1], function_service);
- 
-var i
-function loadDoc(url, cFunction) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      const result = JSON.parse(this.responseText);
-      cFunction(result);
-      console.log(result)
-      console.log(result.service_name)
-    }
-  };
-  xhttp.open("GET", url, true)
-  xhttp.send();
-}
-
-function function_service(result) {
-var desc = JSON.parse(result.long_description);
-var pic  = JSON.parse(result.photo_address);
-console.log(desc)
-  // action goes here
-for(i=0;i<6;i++){
-      if(urlParams[1] == i){
-        document.title = ` ${result.service_name}`;
-          document.getElementById("breadcrumb_subtitle").innerHTML =  `
-          <li class="breadcrumb-item">Services</li>
-          <li class="breadcrumb-item">${result.service_name}</li>
-          
-          `
-        document.getElementById("service_content1").innerHTML = `
-         <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 align-items-start">
-              <h1>
-                ${result.service_name}
-              </h1>
-            </div>
-            <div
-              class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 align-items-start"
-            >
-              <p class="cus_present">
-                <strong>${desc[0]}</strong>
-                <a class="cus_link" href="#">
-                  <span>
-                    Anna Andreoni
-                  </span>
-                </a>
-              </p>
-              <p>
-                <strong>${desc[1]}</strong>
-                <a href="#">
-                  <span>
-                    Autumn Socializing,
-                  </span>
-                </a>
-                <a href="#">
-                  <span>
-                    Milcare Celebrate,
-                  </span>
-                </a>
-                <a href="#">
-                  <span>
-                    Spring
-                  </span>
-                </a>
-              </p>
-            </div>
-
-        `
-        document.getElementById("service_content2").innerHTML = `
-         <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 align-items-start">
-              <p class="cus_content">
-                ${desc[2]}
-              </p>
-              <p class="cus_content">${desc[3]}</p>
-            </div>
-            <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 align-items-start">
-              
-              <p class="cus_content">${desc[4]}</p>
-              <p class="cus_content">${desc[5]}</p>
-            </div>`
-        document.getElementById("gallery").innerHTML = `
-           <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 align-items-center">
-              <img
-                class="img-fluid img-thumbnail"
-                src="../assets/img/${pic[0]}"
-                alt=""
-              />
-          </div>
-
-          <div
-            class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 align-items-center"
-          >
-              <img
-                class="img-fluid img-thumbnail"
-                src="../assets/img/${pic[1]}"
-                alt=""
-              />
-          </div>
-
-          <div
-            class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 align-items-center"
-          >
-              <img
-                class="img-fluid img-thumbnail"
-                src="../assets/img/${pic[2]}"
-                alt=""
-              />
-          </div>
-
-          <div
-            class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 align-items-center"
-          >
-              <img
-                class="img-fluid img-thumbnail"
-                src="../assets/img/${pic[3]}"
-                alt=""
-              />
-          </div>
-        `
-
-
- }
-}
-
+const URL = {
+  ID: "/" + window.location.search.split("=")[1],
+  API: "https://aysa-ojoloo.herokuapp.com/api",
+  EVENTS: "/events",
+  PERSON: "/person",
+  SERVICES: "/services",
 };
+
+fetch(URL.API + URL.SERVICES + URL.ID)
+  .then(function (response) {
+    if (response.status !== 200) {
+      console.log("Fetch response failed. Status Code: " + response.status);
+      return Promise.reject(response);
+    } else {
+      return response.json();
+    }
+  })
+  .then(function (data) {
+    document.title = `${data.service_name}`;
+    var L_description = JSON.parse(data.long_description);
+    var pic = JSON.parse(data.photo_address);
+
+    document.getElementById("breadcrumb_subtitle").innerHTML = `<li class="breadcrumb-item">Services</li><li class="breadcrumb-item">${data.service_name}</li>`;
+
+    document.getElementById("service_content1").innerHTML = `<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 align-items-start"><h1>${data.service_name}</h1></div><div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 align-items-start"><p class="cus_present mt-3"><strong>${L_description[0]} </strong><div id="getPersonFromAPI"></div></p><p><strong>${L_description[1]}</strong><div id="getEventsFromAPI"></div></p></div>`;
+
+    document.getElementById("service_content2").innerHTML = `<div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 align-items-start"><p class="cus_content">${L_description[2]}</p><p class="cus_content">${L_description[3]}</p></div><div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 align-items-start"><p class="cus_content">${L_description[4]}</p><p class="cus_content">${L_description[5]}</p></div>`;
+
+
+    pic.forEach((element) => {
+      document.getElementById("gallery").innerHTML += `<div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 align-items-center"><img class="img-fluid img-thumbnail" src="../assets/img/${element}" alt="${data.service_name}"/></div>`;
+    });
+
+    return fetch(URL.API + URL.SERVICES + URL.ID + URL.EVENTS);
+
+  })
+  .then(function (response) {
+    if (response.status !== 200) {
+      console.log("Fetch response failed. Status Code: " + response.status);
+      return Promise.reject(response);
+    } else {
+      return response.json();
+    }
+  })
+  .then(function (data) {
+    if (data.length != 0) {
+      data.forEach((element) => {
+        document.getElementById(
+          "getEventsFromAPI"
+        ).innerHTML += `<a class="cus_link" href="event.html?event_id=${element.event_id}"><span>${element.event_name}, </span></a>`;
+      });
+    } else {
+      document.getElementById(
+        "getEventsFromAPI"
+      ).innerHTML += `<p><span>Not involved in any events</span></p>`;
+    }
+    return fetch(URL.API + URL.SERVICES + URL.ID + URL.PERSON);
+  })
+  .then(function (response) {
+    if (response.status !== 200) {
+      console.log("Fetch response failed. Status Code: " + response.status);
+      return Promise.reject(response);
+    } else {
+      return response.json();
+    }
+  })
+  .then(function (data) {
+    if (data.length != 0) {
+      data.forEach((element) => {
+        document.getElementById(
+          "getPersonFromAPI"
+        ).innerHTML += `<a class="cus_link" href="member.html?person_id=${element.person_id}"><span>${element.person_name}, </span></a>`;
+      });
+    } else {
+      document.getElementById(
+        "getPersonFromAPI"
+      ).innerHTML += `<p><span>Not involved in any services</span></p>`;
+    }
+  })
+  .catch(function (error) {
+    console.log("Fetch JS failed: ", error);
+  });
