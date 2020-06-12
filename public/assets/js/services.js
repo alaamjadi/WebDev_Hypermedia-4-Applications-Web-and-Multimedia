d57d017bfb26551509cfd7d23322ffb9da916491
MOHAMMAD ALA AMJADI <maa4517@gmail.com>
2020-06-12 03:07:53 +0200

    major and minor fixes to all pages, checking responsiveness of pages, usabilitiy improvments

--- public/assets/js/services.js
+++ public/assets/js/services.js
@@ -18,7 +18,7 @@ fetch(URL.API + URL.SERVICES)
   .then(function (data) {
     data.forEach((element) => {
       pic = JSON.parse(element.photo_address);
-      document.getElementById("service").innerHTML += `<div class="mb-4 align-items-start col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4"><div class="cus_card"><a href="service.html?service_id=${element.service_id}"><div class="card shadow mb-5 bg-white rounded-lg"><div class="view overlay"><img class="card-img-top" src="../assets/img/services_0${element.service_id}.jpg" alt="Service"/></div><div class="card-body" id ="services_card"><h3 class="card-title mb-0 mt-0 text-center"> ${element.service_name} </h3><p class = "text-center mt-2 short_descr"> ${element.short_description}</p><!-- <p class="card-text"></p> --></div></div></a></div></div>`
+      document.getElementById("service").innerHTML += `<div class="mb-4 d-flex justify-content-center col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4"><div class="cus_card"><a href="service.html?service_id=${element.service_id}"><div class="card shadow bg-white rounded-lg h-100"><div class="view overlay"><img class="card-img-top" src="../assets/img/services_0${element.service_id}.jpg" alt="Service"/></div><div class="card-body" id ="services_card"><h3 class="card-title mb-0 mt-0 text-center"> ${element.service_name} </h3><p class="card-text text-justify mt-2">${element.short_description}</p></div></div></a></div></div>`
     });
   })
   .catch(function (error) {
--- public/assets/js/staff.js
+++ public/assets/js/staff.js
@@ -16,10 +16,9 @@ fetch(URL.API + URL.PERSON)
     }
   })
   .then(function (data) {
-    for (i = 0; i < data.length; i++) {
-      data.map((item) => {document.getElementById("person" + i).innerHTML = `<a href="member.html?person_id=${data[i].person_id}"><div class="card hadow mb-5 bg-white rounded-lg"><div class="view overlay"><img class="card-img-top" src="../assets/img/${data[i].photo_address}" alt="Staff1"/></div><div class="card-body" ><h3 class="card-title mb-0 mt-0 text-center">${data[i].person_name}</h3><h3 class="card-title mb-0 mt-0 text-center">${data[i].person_role}</h3><!-- <p class="card-text"></p> --></div></div></a>`;
-      });
-    }
+    data.forEach((element) => {
+     document.getElementById("staff").innerHTML += `<div class="mb-4 d-flex justify-content-center col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3"><div class="cus_card"><a href="member.html?person_id=${element.person_id}"><div class="card shadow bg-white rounded-lg h-100"><div class="view overlay"><img class="card-img-top" src="../assets/img/${element.photo_address}" alt="${element.person_name}"/></div><div class="card-body""><h3 class="card-title mb-0 mt-0 text-center"> ${element.person_name}</h3><h3 class="card-title mb-0 mt-0 text-center"> ${element.person_role}</h3></div></div></a>`
+    });
   })
   .catch(function (error) {
     console.log("Fetch JS failed: ", error);
